8bf4bc9dbc9a7e4f29e78cd9efceb4984dba4ae1
shawn200110 <32945347+shawn200110@users.noreply.github.com>
2024-08-25 16:14:59 -0400

    A lot of stuff, drew response curve and added makePeakFilter helper function

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -37,6 +37,11 @@ enum ChainPositions
     HighCut
 };
 
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
 
 //==============================================================================
@@ -94,8 +99,7 @@ private:
     MonoChain leftChain, rightChain;
 
     void updatePeakFilter(const ChainSettings& chainSettings);
-    using Coefficients = Filter::CoefficientsPtr;
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+    
 
 
     template<int Index, typename ChainType, typename CoefficientType>
