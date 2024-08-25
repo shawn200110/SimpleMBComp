3d3ae4645aed7c0ad08f304bdb202f357f79a087
shawn200110 <32945347+shawn200110@users.noreply.github.com>
2024-08-25 16:32:15 -0400

    Listening for params, check at rate of 60 Hz for a change, and repaint if need be

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,11 +28,24 @@ SimpleMultiBandCompAudioProcessorEditor::SimpleMultiBandCompAudioProcessorEditor
         addAndMakeVisible(comp);
     }
 
+    const auto& params = audioProcessor.getParameters();
+    for (auto param : params)
+    {
+        param->addListener(this);
+    }
+
+    startTimerHz(60);
+
     setSize (600, 400);
 }
 
 SimpleMultiBandCompAudioProcessorEditor::~SimpleMultiBandCompAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for (auto param : params)    
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -145,7 +158,12 @@ void SimpleMultiBandCompAudioProcessorEditor::timerCallback()
     if (parametersChanged.compareAndSetBool(false, true))
     {
         //update the monochain
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+
         //signal a repaint
+        repaint();
     }
 }
 
